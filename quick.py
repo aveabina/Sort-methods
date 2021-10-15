0a68fbeb72f88d4df67ce40d638f9bb2e24a1ecc
aveabina <bainabadmaeva2002@gmail.com>
2021-10-16 01:12:15 +0300

    Added file with QUICK sort

---
+++ quick.py
@@ -0,0 +1,17 @@
+def sort_quick(numbers):
+    if len(numbers) <= 1:
+        return numbers
+    elem = numbers[0]
+    left = []
+    right = []                                        # Быстрая сортировка
+    center = []
+    for i in range(len(numbers)):
+        if numbers[i] > elem:
+            right.append(numbers[i])
+        if numbers[i] == elem:
+            center.append(numbers[i])
+        if numbers[i] < elem:
+            left.append(numbers[i])
+    return sort_quick(left) + center + sort_quick(right)
+sort_quick([10,3,-4,55,-23,5,89,3,23,12,-45])
+print('Done!')
