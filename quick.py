7aa282cf2ba7948bc42da0cbcebb472643130da4
aveabina <bainabadmaeva2002@gmail.com>
2021-10-16 01:49:02 +0300

    Adding output

--- quick.py
+++ quick.py
@@ -12,6 +12,6 @@ def sort_quick(numbers):
             center.append(numbers[i])
         if numbers[i] < elem:
             left.append(numbers[i])
-    return sort_quick(left) + center + sort_quick(right)
+    print(sort_quick(left) + center + sort_quick(right))
 sort_quick([10,3,-4,55,-23,5,89,3,23,12,-45])
-print('Done!')
+print('Done')
