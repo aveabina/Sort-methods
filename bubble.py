ebfcdc8d49a2f37c4775e3116355fcdfa60db860
aveabina <bainabadmaeva2002@gmail.com>
2021-10-17 22:49:10 +0300

    Rework output

--- bubble.py
+++ bubble.py
@@ -11,6 +11,6 @@ numbers = []
 k = int(input('Количество элементов: '))
 for i in range(k):
     numbers.append(randint(-100, 100))
-print('Cписок в начале: ', numbers)
 sort_bubble(numbers)
+print('Cписок в начале: ', numbers)
 print("Список после сортировки: ", numbers)
